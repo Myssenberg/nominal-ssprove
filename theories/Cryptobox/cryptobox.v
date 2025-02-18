eae153c321a8440740c0890a82976607cedf0699
My Marie Nordal Jensen <myssenberg@Mys-MacBook-Pro.local>
2025-02-18 13:19:06 +0100

    cryptobox file added

---
+++ theories/Cryptobox/cryptobox.v
@@ -0,0 +1,33 @@
+(*This is an implementation of the state-separated game-based proof of security for the NaCl crypto_box authenticated encryption scheme.*)
+
+Set Warnings "-notation-overridden,-ambiguous-paths".
+From mathcomp Require Import all_ssreflect all_algebra reals distr realsum
+  fingroup.fingroup solvable.cyclic prime ssrnat ssreflect ssrfun ssrbool ssrnum
+  eqtype choice seq.
+Set Warnings "notation-overridden,ambiguous-paths".
+
+From Coq Require Import Utf8.
+From extructures Require Import ord fset fmap.
+
+From Equations Require Import Equations.
+Require Equations.Prop.DepElim.
+
+Set Equations With UIP.
+
+Set Bullet Behavior "Strict Subproofs".
+Set Default Goal Selector "!".
+Set Primitive Projections.
+
+From NominalSSP Require Import Prelude Group Misc.
+Import PackageNotation.
+
+#[local] Open Scope package_scope.
+
+
+Module crypto_box_scheme.
+
+
+
+
+
+End crypto_box_scheme.
