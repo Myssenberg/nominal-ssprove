3ee3ee39a4d937b703287fcef3dea1bb86796154
Myssenberg <90093429+Myssenberg@users.noreply.github.com>
2025-03-03 15:33:02 +0100

    progress on first lemma

--- theories/Cryptobox/cryptobox.v
+++ theories/Cryptobox/cryptobox.v
@@ -35,7 +35,7 @@ Record crypto_box_scheme :=
     sample_C : code fset0 [interface] C ; (*We might need more logs here*)
 
     gen : 
-      code fset0 [interface] (SK × PK) ;
+      code fset0 [interface] (PK) ;
 
     csetpk : forall (pk : PK),
       code fset0 [interface] unit; (*Unsure of unit is the right term here*)
@@ -47,6 +47,72 @@ Record crypto_box_scheme :=
       code fset0 [interface] M 
   }.
 
+Notation " 'pk p " := (PK p)
+  (in custom pack_type at level 2, p constr at level 20).
+
+Notation " 'pk p " := (PK p)
+  (at level 3) : package_scope.
+
+Notation " 'sk p " := (SK p)
+  (in custom pack_type at level 2, p constr at level 20).
+
+Notation " 'sk p " := (SK p)
+  (at level 3) : package_scope.
+
+Notation " 'm p " := (M p)
+  (in custom pack_type at level 2, p constr at level 20).
+
+Notation " 'm p " := (M p)
+  (at level 3) : package_scope.
+
+Notation " 'c p " := (C p)
+  (in custom pack_type at level 2, p constr at level 20).
+
+Notation " 'c p " := (C p)
+  (at level 3) : package_scope.
+
+
+(*Definition PK_loc (P : crypto_box_scheme): Location := ('option ('pk P) ; 0).*)(*Trying to use option instead of true/false from the paper*)
+
+Definition PK_loc (P : crypto_box_scheme): Location := ('pk P %B ; 0).
+
+Definition SK_loc (P : crypto_box_scheme): Location := (chMap 'pk P 'sk P ; 0). 
+
+
+Definition PKEY_locs_tt (P : crypto_box_scheme):= fset [:: PK_loc P ; SK_loc P]. (*If they're using the same loc, can they share then because Nom-SSP will rename or do we get into trouble?*)
+Definition PKEY_locs_ff (P : crypto_box_scheme):= fset [:: PK_loc P ; SK_loc P].
+
+Definition PKEY_tt (P : crypto_box_scheme):
+  game [interface #val #[gen]: 'unit → 'pk P ]:=
+  [module PKEY_locs_tt P ; 
+    #def #[gen] : PK {
+      '(pk) <- P.(gen) ;;
+      PKLOC <- get PK_loc ;;
+      #put (PK_loc P) := setm PKLOC pk True ;;
+      ret pk
+    }
+  ].
+
+Definition PKEY_ff :=
+  True.
+
+Definition GPKAE_tt_PKEY_tt :=
+  True. (*TEMPORARY*)
+
+Definition GPKAE_tt_PKEY_ff :=
+  False. (*TEMPORARY*)
+
+
+
+
+Definition GPKAE b := if b then GPKAE_PKEY_tt else GPKAE_PKEY_ff.
+
+
+Lemma PK_coll_bound:
+  forall (A : adversary [interface]),
+  AdvFor GPKAE A <=
+  AdvFor GPKAE A.
+Proof.
 
 
 End crypto_box_scheme.
