74089bbafa0696e72a8cc8132ed0c0d39038e9a8
sacc <sacc@itu.dk>
2025-03-24 15:16:41 +0100

    Created the setup for the KEY package.

--- theories/Cryptobox/key.v
+++ theories/Cryptobox/key.v
@@ -25,49 +25,40 @@ Import PackageNotation.
 
 Module KEY.
 
+Variable (n: nat).
 
+Definition Big_N: nat := 2^n.
+Definition Key: choice_type := chFin (mkpos Big_N).
+Definition PK: choice_type := chFin (mkpos Big_N).
+Definition SessionID : choice_type := (PK × PK).
 
-Instance pk_posi p : Positive #|PK p|.
-Proof.
-apply PK_pos. Defined.
+Notation " 'key " := (Key) (in custom pack_type at level 2).
+Notation " 'key " := (Key) (at level 2): package_scope.
 
-Instance sk_posi p : Positive #|SK p|.
-Proof.
-apply SK_pos. Defined.
+Notation " 'pk " := (PK) (in custom pack_type at level 2).
+Notation " 'pk " := (PK) (at level 2): package_scope.
 
-Definition chSet t := chMap t 'unit.
+Notation " 'SID " := (SessionID) (in custom pack_type at level 2).
+Notation " 'SID " := (SessionID) (at level 2): package_scope.
 
-Notation " 'set t " := (chSet t) (in custom pack_type at level 2).
-Notation " 'set t " := (chSet t) (at level 2): package_scope.
+Definition SID_loc : Location := (chMap 'SID 'bool ; 0).
+Definition K_loc : Location := (chMap 'SID 'key ; 1).
 
-Definition h : choice_type := ('set ('pk × 'pk)).
-Definition key : choice_type := ().
-
-Definition K_loc : Location := (chMap 'key E 'bool ; 0). (*How to define the key?*)
-
-
-
-
-Definition KEY_locs_tt := fset [:: K_loc ].  
-Definition KEY_locs_ff := fset [:: K_loc ].
+Definition KEY_locs_tt := fset [:: SID_loc ; K_loc].  
+Definition KEY_locs_ff := fset [:: SID_loc ; K_loc].
 
 Definition SET := 2%N.
+Definition CSET := 3%N.
 Definition GET := 4%N.
 Definition HON := 5%N.
-Definition CSET := 6%N.
-
-
 
 Definition I_KEY_OUT :=
   [interface
-    #val #[ SET ]:  (h × key) → 'unit ;
-    #val #[ GET ]:  (h × key) → 'unit ;
-    #val #[ HON ]:  (h) → 'unit ;(*key option*)
-    #val #[ CSET ]: (h) → 'unit  (*bool option*)
-
+    #val #[ SET ]:  ('SID × 'key) → 'unit ;
+    #val #[ CSET ]: ('SID × 'key) → 'unit ;
+    #val #[ GET ]:  ('SID) → 'key ;
+    #val #[ HON ]:  ('SID) → 'bool
 ].
-
-
 
 Notation "'getNone' n ;; c" :=
   ( v ← get n ;;
@@ -88,15 +79,12 @@ Notation "x ← 'getSome' n ;; c" :=
   format "x  ←  getSome  n  ;;  '//' c")
   : package_scope.
 
-Definition KEY:
-  module (I_KEY_OUT)  := 
-  [module KEY_locs_tt E ;
+(*Definition KEY:
+  module (I_KEY_OUT) := 
+  [module KEY_locs_tt ;
     #def #[ SET ] ('(h, key): 'h  × key) : 'bool {
      
     }
-  ].
-
-
-
+  ].*)
 
 End KEY.
