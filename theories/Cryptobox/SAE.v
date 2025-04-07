9d8b3376c1299ccfacc4aeafdf5e46f6d32e5746
sacc <sacc@itu.dk>
2025-04-07 16:41:10 +0200

    SAE package done.

--- theories/Cryptobox/SAE.v
+++ theories/Cryptobox/SAE.v
@@ -32,12 +32,9 @@ Module SAE.
 Notation " 'T c " := (c) (in custom pack_type at level 2, c constr at level 20).
 Notation " 'T c " := (c) (at level 2): package_scope.
 
-(*Definition M_loc (E: NBSES_scheme): Location := (chMap 'n E ('m E × 'c E) ; 0). *)
 Definition M_loc (n : finType) (m c : choice_type) `{Positive #|n|} : Location := (chMap 'fin #|n| (m × c) ; 0).
 Definition K_loc (k : finType) `{Positive #|k|} : Location := ('option 'fin #|k| ; 1).
 
-(*Definition SAE_locs_tt (E : NBSES_scheme):= fset [::  M_loc E]. (*If they're using the same loc, can they share then because Nom-SSP will rename or do we get into trouble?*)
-Definition SAE_locs_ff (E : NBSES_scheme):= fset [::  M_loc E].*)
 Definition SAE_locs_tt (m c : choice_type) (n k : finType) `{Positive #|n|} `{Positive #|k|} := fset [::  M_loc n m c ; K_loc k]. (*If they're using the same loc, can they share then because Nom-SSP will rename or do we get into trouble?*)
 Definition SAE_locs_ff (m c : choice_type) (n k : finType) `{Positive #|n|} `{Positive #|k|} := fset [::  M_loc n m c ; K_loc k]. (*If they're using the same loc, can they share then because Nom-SSP will rename or do we get into trouble?*)
 
@@ -48,11 +45,11 @@ Definition DEC := 4%N.
 Definition I_SAE_OUT (n m c: choice_type) :=
   [interface
     #val #[ GEN ]: 'unit → 'unit ;    
-    #val #[ ENC ]: ('T m × 'T n) → 'unit (*'T c*)  ;
-    #val #[ DEC ]: ('T c × 'T n) → 'unit (*'T m*) 
+    #val #[ ENC ]: ('T m × 'T n) → 'T c  ;
+    #val #[ DEC ]: ('T c × 'T n) → 'T m 
 ].
 
-Definition SAE (b : bool) (m c : choice_type) (n k : finType) `{Positive #|n|} `{Positive #|k|} (sample_K : code fset0 [interface] 'fin #|k|) (sample_C : code fset0 [interface] c) (enc : forall (m : m) (k : k) (n : n), code fset0 [interface] c) (dec : forall (c : c) (k : k) (n : n), code fset0 [interface] m):
+Definition SAE (b : bool) (m c : choice_type) (n k : finType) `{Positive #|n|} `{Positive #|k|} (sample_K : code fset0 [interface] 'fin #|k|) (sample_C : code fset0 [interface] c) (enc : forall (m : m) (k : 'T 'fin #|k|) (n : 'T 'fin #|n|), code fset0 [interface] c) (dec : forall (c : c) (k : 'T 'fin #|k|) (n : 'fin #|n|), code fset0 [interface] m):
   game (I_SAE_OUT 'fin #|n| m c)  := 
   [module SAE_locs_tt m c n k ;
     #def #[ GEN ] (_ : 'unit) : ('unit) {
@@ -65,7 +62,7 @@ Definition SAE (b : bool) (m c : choice_type) (n k : finType) `{Positive #|n|} `
       | Some k' => ret (Datatypes.tt : 'unit)
       end
     } ;
-    #def #[ ENC ] ('(m', n') : ('T m × 'T 'fin #|n|)) : 'unit {
+    #def #[ ENC ] ('(m', n') : ('T m × 'T 'fin #|n|)) : ('T c) {
       MLOC ← get M_loc n m c ;;
       #assert MLOC n' == None ;;
       KLOC ← get K_loc k ;;
@@ -74,15 +71,13 @@ Definition SAE (b : bool) (m c : choice_type) (n k : finType) `{Positive #|n|} `
       if (b) then
        c' ← sample_C ;;
        #put (M_loc n m c) := setm MLOC (n') (m', c') ;;
-       ret (Datatypes.tt : 'unit)
-       (*ret c'*)
+       ret c'
       else
-       (*c' ← enc m' k' n' ;;
-       #put (M_loc n m c) := setm MLOC (n') (m', c') ;;*)
-       ret (Datatypes.tt : 'unit)
-       (*ret c'*)
+       c' ← enc m' k' n' ;;
+       #put (M_loc n m c) := setm MLOC (n') (m', c') ;;
+       ret c'
     } ;
-    #def #[ DEC ] ('(c', n') : ('T c × 'T 'fin #|n|)) : 'unit {
+    #def #[ DEC ] ('(c', n') : ('T c × 'T 'fin #|n|)) : ('T m) {
       KLOC ← get K_loc k ;;
       #assert (isSome KLOC) as someKey ;;
       let k' := getSome KLOC someKey in
@@ -90,12 +85,10 @@ Definition SAE (b : bool) (m c : choice_type) (n k : finType) `{Positive #|n|} `
        MLOC ← get M_loc n m c ;;
        #assert isSome (MLOC n') as MC ;;
        let (m', c'') := getSome (MLOC n') MC in 
-       ret (Datatypes.tt : 'unit)
-       (*ret m'*)
+       ret m'
       else
-       (*m' ← dec c' k' n' ;;*)
-       ret (Datatypes.tt : 'unit)
-       (*ret m'*)
+       m' ← dec c' k' n' ;;
+       ret m'
     }
   ].
 
