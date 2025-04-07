6ac74893914cfcdfa0bc90aef98d4d68df51bbe1
sacc <sacc@itu.dk>
2025-04-07 10:54:17 +0200

    Rewrote GEN method to match EasyCrypt version.

--- theories/Cryptobox/SAE.v
+++ theories/Cryptobox/SAE.v
@@ -34,29 +34,36 @@ Notation " 'T c " := (c) (at level 2): package_scope.
 
 (*Definition M_loc (E: NBSES_scheme): Location := (chMap 'n E ('m E × 'c E) ; 0). *)
 Definition M_loc (n : choice_type) (m : choice_type) (c : choice_type) : Location := (chMap n (m × c) ; 0).
+Definition K_loc (k : choice_type) : Location := ('option k ; 1).
 
 (*Definition SAE_locs_tt (E : NBSES_scheme):= fset [::  M_loc E]. (*If they're using the same loc, can they share then because Nom-SSP will rename or do we get into trouble?*)
 Definition SAE_locs_ff (E : NBSES_scheme):= fset [::  M_loc E].*)
-Definition SAE_locs_tt (n : choice_type) (m : choice_type) (c : choice_type) := fset [::  M_loc n m c]. (*If they're using the same loc, can they share then because Nom-SSP will rename or do we get into trouble?*)
-Definition SAE_locs_ff (n : choice_type) (m : choice_type) (c : choice_type) := fset [::  M_loc n m c]. (*If they're using the same loc, can they share then because Nom-SSP will rename or do we get into trouble?*)
+Definition SAE_locs_tt (k : choice_type) (n : choice_type) (m : choice_type) (c : choice_type) := fset [::  M_loc n m c ; K_loc k]. (*If they're using the same loc, can they share then because Nom-SSP will rename or do we get into trouble?*)
+Definition SAE_locs_ff (k : choice_type) (n : choice_type) (m : choice_type) (c : choice_type) := fset [::  M_loc n m c ; K_loc k]. (*If they're using the same loc, can they share then because Nom-SSP will rename or do we get into trouble?*)
 
-Definition GEN := 1%N.
-Definition ENC := 2%N.
-Definition DEC := 3%N.
+Definition GEN := 2%N.
+Definition ENC := 3%N.
+Definition DEC := 4%N.
 
 Definition I_SAE_OUT (k : choice_type) (n : choice_type) (m : choice_type) (c : choice_type) :=
   [interface
-    #val #[ GEN ]: 'unit → 'T k (*;    
+    #val #[ GEN ]: 'unit → 'unit (*;    
     #val #[ ENC ]: ('T m × 'T n) → 'T c ;
     #val #[ DEC ]: ('T c × 'T n) → 'T m*) 
 ].
 
 Definition SAE (k : choice_type) (n : choice_type) (m : choice_type) (c : choice_type) (b : bool) (sample_K : code fset0 [interface] k) :
   game (I_SAE_OUT k n m c)  := 
-  [module SAE_locs_tt n m c ;
-    #def #[ GEN ] (_ : 'unit) : ('T k) {
-      key ← sample_K ;;
-      ret key
+  [module SAE_locs_tt k n m c ;
+    #def #[ GEN ] (_ : 'unit) : ('unit) {
+      KLOC ← get K_loc k ;;
+      match KLOC with
+      | None =>
+        k' ← sample_K ;;
+        #put (K_loc k) := Some k' ;;
+        ret (Datatypes.tt : 'unit)
+      | Some k' => ret (Datatypes.tt : 'unit)
+      end
     }
   ].
 
