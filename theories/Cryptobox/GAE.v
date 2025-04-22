7171ff527cec95c79e8b0fe5179dd03cfcfa6135
sacc <sacc@itu.dk>
2025-04-22 11:55:34 +0200

    KEY package can now take different schemes and GAE is proven as ValidPackage.

--- theories/Cryptobox/GAE.v
+++ theories/Cryptobox/GAE.v
@@ -41,34 +41,26 @@ Definition DEC := 53%N.
 
 Definition I_GAE_OUT (E : NBSES_scheme) (N : NIKE_scheme) :=
   [interface
-    #val #[ SET ]: ('SID N × 'shared_key N) → 'unit ;
-    #val #[ CSET ]: ('SID N × 'shared_key N) → 'unit ;
+    #val #[ SET ]: ('SID N × 'fin #|E.(NBSES.Shared_Key)|) → 'unit ;
+    #val #[ CSET ]: ('SID N × 'fin #|E.(NBSES.Shared_Key)|) → 'unit ;
     #val #[ ENC ]: ((('pk N × 'pk N) × 'm E) × 'n E) → 'c E ;
     #val #[ DEC ]: ((('pk N × 'pk N) × 'c E) × 'n E) → 'm E 
 ].
 
-Definition I_GAE_ID_COMP (N : NIKE_scheme) :=
+Definition I_GAE_ID_COMP (N : NIKE_scheme) (E : NBSES_scheme) :=
   [interface
-    #val #[ SET ]: ('SID N × 'shared_key N) → 'unit ;
-    #val #[ CSET ]: ('SID N × 'shared_key N) → 'unit
+    #val #[ SET ]: ('SID N × 'fin #|E.(NBSES.Shared_Key)|) → 'unit ;
+    #val #[ CSET ]: ('SID N × 'fin #|E.(NBSES.Shared_Key)|) → 'unit
 ].
 
 #[export] Hint Unfold I_GAE_OUT I_GAE_ID_COMP I_AE_IN I_AE_OUT I_KEY_OUT I_NIKE_IN I_NIKE_OUT : in_fset_eq.
 
 Definition GAE (E : NBSES_scheme) (N : NIKE_scheme) (b : 'bool):
-  raw_module := (AE b E N || ID (I_GAE_ID_COMP N)) ∘ (KEY b N).
+  raw_module := (AE b E N || ID (I_GAE_ID_COMP N E)) ∘ (KEY b N (NBSES_to_SGEN E)).
 
-(*Lemma GAE_valid (E : NBSES_scheme) (N: NIKE_scheme) (b : 'bool) :
+Lemma GAE_valid (E : NBSES_scheme) (N: NIKE_scheme) (b : 'bool) :
   ValidPackage (GAE E N b).(loc) [interface] (I_GAE_OUT E N) (GAE E N b).
 Proof.
-unfold GAE. nssprove_valid. fset_solve.*)
-
-(*Definition GAE (E : NBSES_scheme) (N : NIKE_scheme) (b : 'bool) (pk: finType) `{Positive #|pk|} :
-  raw_module := (AE b E pk || ID (I_GAE_ID_COMP N 'fin #|pk|)) ∘ (KEY b N).
-
-Lemma GAE_valid (E : NBSES_scheme) (N: NIKE_scheme) (b : 'bool) (pk: finType) `{Positive #|pk|} :
-  ValidPackage (GAE E N b pk).(loc) [interface] (I_GAE_OUT E N 'fin #|pk|) (GAE E N b pk).
-Proof.
-unfold GAE. nssprove_valid. fset_solve.*)
+unfold GAE. nssprove_valid. Qed.
 
 End GAE.
