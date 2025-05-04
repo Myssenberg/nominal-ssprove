c0cbc727179ca8f58cf9f87fb4aca3d6dc9501cb
sacc <sacc@itu.dk>
2025-05-04 10:37:54 +0200

    The GH game is proven as a valid package.

--- theories/Cryptobox/GH.v
+++ theories/Cryptobox/GH.v
@@ -42,19 +42,45 @@ Definition I_GH_ID_COMP (E: NBSES_scheme) (N : NIKE_scheme) :=
   [interface
     #val #[ SET ]: (('pk N × 'pk N) × 'fin #|E.(NBSES.Shared_Key)|) → 'unit  ;
     #val #[ CSET ]: (('pk N × 'pk N) × 'fin #|E.(NBSES.Shared_Key)|) → 'unit ;
-    #val #[ GET ]: ('pk N × 'pk N) → 'k E (* ;
-    #val #[ HON ]: ('pk N × 'pk N)  → 'option 'bool *)
+    #val #[ GET ]: ('pk N × 'pk N) → 'k E
 ].
 
-(* #[export] Hint Unfold I_GH_OUT I_GH_ID_COMP I_AE_IN I_AE_OUT I_KEY_OUT I_NIKE_IN I_NIKE_OUT I_SAE_OUT I_HYBRID_IN I_HYBRID_OUT : in_fset_eq. *)
+Definition I_GH_FST (E: NBSES_scheme) (N : NIKE_scheme) :=
+  [interface
+    #val #[ GEN ]: 'unit → 'unit ; 
+    #val #[ SENC ]: ('m E × 'n E) → 'c E  ;
+    #val #[ SDEC ]: ('c E × 'n E) → 'm E ;
+    #val #[ ENC ]: ((('pk N × 'pk N) × 'm E) × 'n E) → 'c E ;
+    #val #[ DEC ]: ((('pk N × 'pk N) × 'c E) × 'n E) → 'm E 
+].
 
-(* Temporary attempt at composing game *)
+Definition I_GH_SND (E: NBSES_scheme) (N : NIKE_scheme) :=
+  [interface
+    #val #[ SET ]: (('pk N × 'pk N) × 'fin #|E.(NBSES.Shared_Key)|) → 'unit  ;
+    #val #[ CSET ]: (('pk N × 'pk N) × 'fin #|E.(NBSES.Shared_Key)|) → 'unit ;
+    #val #[ GET ]: ('pk N × 'pk N) → 'k E ;
+    #val #[ GEN ]: 'unit → 'unit ; 
+    #val #[ SENC ]: ('m E × 'n E) → 'c E  ;
+    #val #[ SDEC ]: ('c E × 'n E) → 'm E 
+].
+
+Definition I_GH_TRD (E: NBSES_scheme) (N : NIKE_scheme) :=
+  [interface
+    #val #[ SET ]: (('pk N × 'pk N) × 'fin #|E.(NBSES.Shared_Key)|) → 'unit  ;
+    #val #[ CSET ]: (('pk N × 'pk N) × 'fin #|E.(NBSES.Shared_Key)|) → 'unit ;
+    #val #[ GET ]: ('pk N × 'pk N) → 'k E ;
+    #val #[ GET ]: ('pk N × 'pk N) → 'k E ;
+    #val #[ HON ]: ('pk N × 'pk N)  → 'option 'bool
+].
+
+#[export] Hint Unfold I_GH_OUT I_GH_ID_COMP I_HYBRID_IN I_HYBRID_OUT I_GH_FST I_GH_SND I_GH_TRD I_AE_IN I_AE_OUT I_KEY_OUT I_SAE_OUT : in_fset_eq.
+
 Definition GH (E : NBSES_scheme) (N : NIKE_scheme) i (b : 'bool):
-  raw_module := (HYBRID E N i) ∘ (AE b E N || SAE b E || (ID (I_GH_ID_COMP E N)) ∘ (KEY true N (NBSES_to_SGEN E))).
+  raw_module := (HYBRID E N i) ∘ (((ID (I_GH_ID_COMP E N) || ID (I_GH_FST E N)) ∘ (AE b E N || ID (I_GH_SND E N)) ∘ (SAE b E || ID (I_GH_TRD E N))) ∘ (KEY true N (NBSES_to_SGEN E))).
 
-Lemma GH_valid (E : NBSES_scheme) (N: NIKE_scheme) i (b : 'bool) :
+Lemma GAE_valid (E : NBSES_scheme) (N: NIKE_scheme) i (b : 'bool) :
   ValidPackage (GH E N i b).(loc) [interface] (I_GH_OUT E N) (GH E N i b).
 Proof.
-unfold GH. nssprove_valid. About KEY.
+unfold GH. nssprove_valid. Qed.
 
 End GH.
